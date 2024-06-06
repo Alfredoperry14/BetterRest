3f2754d6bd93e24bd9792b7c07cfc6d369ae52ab
alfredoperry14 <alfredoperry14@gmail.com>
2024-06-06 15:02:17 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alfredo Perry on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
